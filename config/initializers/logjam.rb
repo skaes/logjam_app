ee4c7aea0ea402b4dd8b3b936ae75df3dfa885b7
Stefan Kaes <skaes@railsexpress.de>
2010-08-12 11:06:00 +0200

    support import threshold. small sites can import all requests.

--- config/initializers/logjam.rb
+++ config/initializers/logjam.rb
@@ -2,7 +2,12 @@
 #
 
 module Logjam
+  # Set base url when embedding logam into your main app
   # Logjam.base_url = 'stats'
+
+  # Set import_threshold. Requests fast than the threshold will not be stored in mongo.
+  # Their performance data is added to the stats, though. Default is 0.
+  # Logjam.import_threshold = 500
 
   # Make sure to enable matchers appropriate for the log files that will be imported.
   # The sample log file included with LogJam is in basic time bandit format.
