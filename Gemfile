cf7d34f65e275da8d9b95f91434fea2494a7af68
Stefan Kaes <skaes@railsexpress.de>
2011-03-05 12:30:30 +0100

    rails 3 port

--- Gemfile
+++ Gemfile
@@ -1,11 +1,20 @@
 source :rubygems
+
 gem "rake"
-gem "rails", '=2.3.11'
+gem "rails", '~>3.0'
 gem "mongo_ext"
 gem "mongo"
 gem "bson_ext"
 gem "eventmachine"
 gem "amqp"
-gem "logjam_logger"
+gem "logjam_logger", :git => "git://github.com/skaes/logjam_logger.git"
 gem "json"
 gem "em-websocket"
+
+group :development do
+  gem "wirble"
+end
+
+group :test do
+  gem "redgreen"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LogjamApp::Application.config.secret_token = '452556f6f2ca7bc468b8ca4ec178f4f5e3d76ce8a6ccaf312175224f29361e48005738a3dae66ec37f9231d7fef2ca25442cb1072e3035a9532e70f081a41ebd'
