8502fe9b45dadd728ca7b3d02ca2dd63d07c586c
Stefan Kaes <stefan.kaes@xing.com>
2013-09-26 09:38:41 +0200

    updated logjam initializer

--- config/initializers/logjam.rb
+++ config/initializers/logjam.rb
@@ -1,19 +1,23 @@
-# CONFIGURATION
-#
-
 module Logjam
-  # Set import_threshold. Requests fast than the threshold will not be stored in mongo.
-  # Their performance data is added to the stats, though. Default is 0.
-  # Logjam.import_threshold = Rails.env == "development" ? 0 : 750
+  # Set the import threshold (ms). Requests with a response time lower
+  # than the threshold will not be stored in mongo. Their performance
+  # data is added to the stats, though.
+  self.import_threshold = Rails.env.development? ? 0 : 750
 
-  # Set how many days we keep request details
-  Logjam.request_cleaning_threshold = 7
+  # Set how many days we keep request details stored in mongo.
+  self.request_cleaning_threshold = 7
 
-  # Set how many days we keep statistics
-  Logjam.database_cleaning_threshold = 18*7 # 18 weeks
+  # Set how many days we keep statistics around.
+  self.database_cleaning_threshold = 14
 
-  # Set how often accumulated stats are flushed to the database
-  # this also determines the stream update interval
-  Logjam.database_flush_interval = 1
+  # Set how often accumulated stats are flushed to the database.
+  # This setting also determines the stream update interval
+  self.database_flush_interval = 1
 
+  # Set the bind ip for logjam daemons. Defaults to "127.0.0.1",
+  # which is appropriate for single machine installs. If you are distributing
+  # daemon processes across several machines, you must set it to "0.0.0.0."
+  # If you change this setting in development mode, you must regenerate the
+  # service definitions by running 'rake logjam:daemons:install'.
+  self.bind_ip = "127.0.0.1"
 end
