2c4968289feb77482bb89a128a54775af768cbec
Stefan Kaes <stefan.kaes@xing.com>
2013-09-29 20:31:04 +0200

    switch default log level to info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ LogjamApp::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  config.log_level = :debug
+  # config.log_level = :debug
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
