51723937d0ce365e1ca349698e133407fd94ad39
Stefan Kaes <stefan.kaes@xing.com>
2015-05-03 16:24:44 +0200

    starting to drop support for ruby importers

--- config/initializers/logjam_streams.rb
+++ config/initializers/logjam_streams.rb
@@ -1,28 +1,17 @@
 # logjam importer streams
 # developent streams are skipped in production
 module Logjam
-  # ---------------------------- #
-  # live performance data stream #
-  # ---------------------------- #
+
+  # ----------------------- #
+  # performance data stream #
+  # ----------------------- #
 
   livestream "development"
   livestream "production"
 
-  # ------------------------- #
-  # config for ruby importers #
-  # ------------------------- #
-
-  # stream "logjam-development" do
-  #   importer { type :zmq; port 9605 }
-  # end
-
-  # stream "logjam-production" do
-  #   importer { type :zmq; port 9605 }
-  # end
-
-  # --------------------- #
-  # config for C importer #
-  # --------------------- #
+  # ----------------------- #
+  #  config for C importer  #
+  # ----------------------- #
 
   self.devices = %w[tcp://127.0.0.1:9606]
 
