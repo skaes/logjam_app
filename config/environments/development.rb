3b46ceea7e26dfa5a13837fdae0ba83c33230872
Stefan Kaes <stefan.kaes@xing.com>
2016-03-02 15:26:46 +0100

    added missing initializer for assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,13 +17,30 @@ LogjamApp::Application.configure do
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
+  # Raise an error on page load if there are pending migrations.
+  # config.active_record.migration_error = :page_load
+
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
   # config.log_tags = [:uuid]
 
-  # config.assets.digest = true
-  # config.assets.prefix = "/dev-assets"
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 
 end
 
